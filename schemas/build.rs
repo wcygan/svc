1db50928028a245e9775876e7244f7ae8416ac46
Will Cygan <wcygan.io@gmail.com>
2023-11-11 16:53:55 -0600

    add the proto schemas

---
+++ schemas/build.rs
@@ -0,0 +1,19 @@
+extern crate glob;
+
+use glob::glob;
+use std::error::Error;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let proto_files: Vec<String> = glob("protos/**/*.proto")?
+        .filter_map(Result::ok)
+        .map(|path| path.display().to_string())
+        .collect();
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile(&proto_files, &["protos/"])?;
+
+    Ok(())
+}
